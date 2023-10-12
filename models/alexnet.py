dffd61188a5f2e1b21b8850e1881106ee3b84464
kjw <yoonlight12@gmail.com>
2023-10-12 23:42:29 +0000

    refactor(model): refactor alextnet and lenet

--- models/alexnet.py
+++ models/alexnet.py
@@ -7,30 +7,45 @@ from torch import nn
 class AlexNet(nn.Module):
     def __init__(self):
         super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.flatten = nn.Flatten()
         self.net = nn.Sequential(
-            nn.LazyConv2d(96, kernel_size=11, stride=4, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LazyConv2d(256, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LazyConv2d(384, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.LazyConv2d(384, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.LazyConv2d(256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Flatten(),
-            nn.LazyLinear(4096),
+            nn.Linear(6400, 4096),
             nn.ReLU(),
             nn.Dropout(p=0.5),
-            nn.LazyLinear(4096),
+            nn.Linear(4096, 4096),
             nn.ReLU(),
             nn.Dropout(p=0.5),
-            nn.LazyLinear(1),
-            nn.Sigmoid()
+            nn.Linear(4096, 1),
         )
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.flatten(x)
         return self.net(x)
